e8dbd3c0eb0efec5ca0eb1b29bb0692306afaaa1
Stefano <stefanocrocco.sc@outlook.com>
2025-10-05 01:00:31 +0200

    Delete data directory

--- data/Employees_Department.sql
+++
@@ -1,4 +0,0 @@
-select department, count(Employee_ID) as Employee_Count
-from `employees-473322.Employees.Employees`
-group by Department
-order by employee_count desc;
--- data/count_employees.sql
+++
@@ -1,2 +0,0 @@
-select count (employee_id)
-from `employees-473322.Employees.Employees`;
